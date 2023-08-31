bd321b33955912ee550be145e4b23f58ae10a8a1
fermasia <fermasia@hotmail.com>
2023-08-31 01:03:49 -0300

    Final update, puntos 1,2,3,4,5

--- datagen.py
+++ datagen.py
@@ -5,6 +5,8 @@ from dataaug import SmilesEnumerator  # Assuming you have this library for data 
 
 smiles_dict = {'#': 1, '(': 2, ')': 3, '+': 4, '-': 5, '/': 6, '1': 7, '2': 8, '3': 9, '4': 10, '5': 11, '6': 12, '7': 13, '8': 14, '=': 15, 'C': 16, 'F': 17, 'H': 18, 'I': 19, 'N': 20, 'O': 21, 'P': 22, 'S': 23, '[': 24, '\\': 25, ']': 26, '_': 27, 'c': 28, 'Cl': 29, 'Br': 30, 'n': 31, 'o': 32, 's': 33, '@': 34, '.': 35, 'a': 36, 'B': 37, 'e': 38, 'i': 39, '9': 40, '10': 41, '11': 42}
 
+#smiles_dict = {1: 'c', 2: 'C', 3: '(', 4: ')', 5: '1', 6: 'O', 7: '2', 8: 'N',  9: '=', 10: '3', 11: '[', 12: ']', 13: 'n', 14: '@', 15: '4', 16: 'H', 17: '-', 18: '/', 19: 'l', 20: '.', 21: '+', 22: 'F', 23: '5', 24: 'o',  25: 'B', 26: 'r', 27: 'S', 28: '\\', 29: '#', 30: 's', 31: '6', 32: 'I',  33: 'P', 34: 'e', 35: '7', 36: '8', 37: 'i', 38: 'a'}
+
 def smiles_to_seq(smiles, seq_length, char_dict=smiles_dict):
     """ Tokenize characters in smiles to integers
     """
@@ -63,6 +65,9 @@ class DataGenerator(keras.utils.Sequence):
             padded_X = self.pad_sequences(augmented_X)
         else:
             padded_X = self.pad_sequences(batch_X)
+            
+        #tokenize padded X
+        padded_X = [smiles_to_seq(x, seq_length=self.seq_length, char_dict=smiles_dict) for x in padded_X]
 
         return np.array(padded_X), np.array(batch_y)
 
